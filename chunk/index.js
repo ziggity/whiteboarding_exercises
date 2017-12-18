7d56fa7966af111238c0f23684350c5285c5a07f
zach beecher <zachbeecher@gmail.com>
2017-12-17 20:05:19 -0800

    chunk array completed

--- chunk/index.js
+++ chunk/index.js
@@ -8,6 +8,14 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size){
+  const chunked = [];
+  let index = 0;
 
-module.exports = chunk;
+  while(index<array.length){
+    chunked.push(array.slice(index, index + size));
+    index+=size;
+  }
+  return chunked;
+}
+ chunk([50,1, 2, 3, 4, 50], 2);
