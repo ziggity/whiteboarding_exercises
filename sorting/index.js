f1f96e1e7075e8edab90d9c96d1635a148ad7a65
zach beecher <zachbeecher@gmail.com>
2017-12-25 22:12:09 -0800

    merge sort

--- sorting/index.js
+++ sorting/index.js
@@ -15,8 +15,24 @@ return arr;
 }
 
 function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin = i;
 
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j;
+      }
+    }
+    if (indexOfMin !== i) {
+      let lesser = arr[indexOfMin];
+      arr[indexOfMin] = arr[i];
+      arr[i] = lesser;
+    }
+  }
+  return arr;
 }
+
+selectionSort([5, 3, 2, 1, 55, 3, 4, 3]);
 
 function mergeSort(arr) {
 
